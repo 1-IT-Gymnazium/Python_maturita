ef828c407a87ce7551c303fff51c3617cf0397e1
Erik Diegel <1034@student.itgymnazium.cz>
2023-12-12 15:38:31 +0100

    Added draw method to tiles.py

--- tiles.py
+++ tiles.py
@@ -1,9 +1,17 @@
+import pygame.draw
+
 from settings import TILE_SIZE
 
 
 class Tile:
-    def __init__(self, x, y, height=TILE_SIZE, width=TILE_SIZE):
+    def __init__(self, x, y, height=TILE_SIZE, width=TILE_SIZE, surface=pygame.display.get_surface()):
         self.x = x
         self.y = y
         self.height = height
         self.width = width
+        self.animations = []
+        self.surface = surface
+
+    def draw(self):
+        rect = pygame.draw.rect(self.surface, "green", (self.x, self.y, self.width, self.height), 0)
+        self.display_surface.blit(self.surface, rect)
